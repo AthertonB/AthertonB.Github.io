45f7a26bc7705c65ac7754d2700cf33f3faf712d
AthertonB <batherton@collegeforcreativestudies.edu>
2016-09-28 00:08:58 -0400

    final site upload

--- interact3p1/scripts/app.js
+++ interact3p1/scripts/app.js
@@ -1,35 +1,16 @@
 $(document).ready(function () {
-/*day/night background cycle*/
-if (timer.hour >= 6 && timer.hour <= 21) {
-    $("body").css("background-color", "lightblue")
-} else {
-    $("body").css("background-color", "darkblue")
-}
+    /*day/night background cycle*/
+    if (timer.hour >= 6 && timer.hour <= 21) {
+        $("body").css("background-color", "lightblue")
+    } else {
+        $("body").css("background-color", "darkblue")
+    }
+    if (timer.hour >= 6 && timer.hour <= 21) {
+        $(".sun").css("background-color", "yellow")
+    } else {
+        $(".sun").css("background-color", "lightgrey")
+    }
     setInterval(function () {
-                $("sky").html(timer.month + '/' + timer.date + '/' + timer.year + ' ' + timer.hour + ':' + timer.minutes + ':' + timer.seconds + ' ' + timer.meridiem );
+        $("sky").html(timer.month + '/' + timer.date + '/' + timer.year + ' ' + timer.hour + ':' + timer.minutes + ':' + timer.seconds + ' ' + timer.meridiem);
     }, 1000);
 });
-
-
-if (timer.hour >=6 && timer.hour <= 21) {
-    AnimateRotate();
-} else {
-    AnimateRotate();
-}
-
-function AnimateRotate(d) {
-  var elem = $("sun");
-
-  $({
-    deg: 0
-  }).animate({
-    deg: d
-  }, {
-    duration: 4000,
-    step: function(now) {
-      elem.css({
-        transform: "rotate(" + now + "deg)"
-      });
-    }
-  });
-}
